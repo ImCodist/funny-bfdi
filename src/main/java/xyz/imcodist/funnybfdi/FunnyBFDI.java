ae4f2d647816d5cd0259ecaa6fb7d1f392f0fec0
codist <50346006+ImCodist@users.noreply.github.com>
2023-10-18 21:26:46 -0400

    Update to 1.1.0 and made purely client side.

--- src/main/java/xyz/imcodist/funnybfdi/FunnyBFDI.java
+++ src/main/java/xyz/imcodist/funnybfdi/FunnyBFDI.java
@@ -1,7 +1,7 @@
 package xyz.imcodist.funnybfdi;
 
 import eu.midnightdust.lib.config.MidnightConfig;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
@@ -11,12 +11,11 @@ import org.slf4j.LoggerFactory;
 import xyz.imcodist.funnybfdi.other.Config;
 import xyz.imcodist.funnybfdi.other.MouthManager;
 
-public class FunnyBFDI implements ModInitializer {
+public class FunnyBFDI implements ClientModInitializer {
 	public static final String MOD_ID = "funnybfdi";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		LOGGER.info("You swear you heard \"New Friendly\" begin to play in your head.");
 
 		MidnightConfig.init(MOD_ID, Config.class);
